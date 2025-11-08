9d4ffa6dc2febfe949eaa618814bcd6047132f33
shubhamvasoya <shubhamvasoya2211@gmail.com>
2025-11-08 14:07:38 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(
+    name="mental-health-chatbot",
+    version="0.1.0",
+    author="Shubham",
+    packages=find_packages(),
+    install_requires=[]
+)
