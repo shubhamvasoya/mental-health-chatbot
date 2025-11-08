827a46f0f2d37b5d8b7e5df0c4ce6126536b77c6
shubhamvasoya <shubhamvasoya2211@gmail.com>
2025-11-08 13:41:37 +0530

    folder structure folder added

---
+++ templete.sh
@@ -0,0 +1,13 @@
+mkdir -p src
+mkdir -p resources
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch resources/trials.ipynb
+touch requirements.txt
+
+echo "File structure created successfully."
