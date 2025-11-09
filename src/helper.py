b20262452ef98dbc164e1226efacc29b63406ee1
shubhamvasoya <shubhamvasoya2211@gmail.com>
2025-11-09 14:37:02 +0530

    Removed Test files

--- src/helper.py
+++ src/helper.py
@@ -5,7 +5,6 @@ from typing import List
 from langchain.schema import Document
 
 
-#Extract Data From the PDF File
 def load_pdf_file(data):
     loader= DirectoryLoader(data,
                             glob="*.pdf",
@@ -31,7 +30,6 @@ def filter_to_minimal_docs(docs: List[Document]) -> List[Document]:
 
 
 
-#Split the Data into Text Chunks
 def text_split(extracted_data):
     text_splitter=RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=20)
     text_chunks=text_splitter.split_documents(extracted_data)
@@ -39,7 +37,6 @@ def text_split(extracted_data):
 
 
 
-#Download the Embeddings from HuggingFace 
 def download_hugging_face_embeddings():
-    embeddings=HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')  #this model return 384 dimensions
+    embeddings=HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
     return embeddings
